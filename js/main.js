66e81e77e47bed0679d33c5e80fcf9fe61cc65f4
LX862 <kenny.mh.hui@gmail.com>
2022-03-28 17:41:05 +0800

    Removing random things is good, totally.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
-	"use strict";
-	var window_width 	 = $(window).width(),
-	window_height 		 = window.innerHeight,
-	header_height 		 = $(".default-header").height(),
-	header_height_static = $(".site-header.static").outerHeight(),
-	fitscreen 			 = window_height - header_height;
-	$(".fullscreen").css("height", window_height)
-	$(".fitscreen").css("height", fitscreen);
-	
-	/* ----- Dynamic Window resize ----- */
-	window.addEventListener("resize", setWindowSize); //Register event on window resize
-	function setWindowSize() {
-		window_width 	     = $(window).width(),
-		window_height 		 = window.innerHeight,
-		header_height 		 = $(".default-header").height(),
-		header_height_static = $(".site-header.static").outerHeight(),
-		fitscreen 			 = window_height - header_height;
-		$(".fullscreen").css("height", window_height)
-		$(".fitscreen").css("height", fitscreen);
-	}
-	
+    "use strict";
+    var window_width = $(window).width(),
+        window_height = window.innerHeight,
+        header_height = $(".default-header").height(),
+        header_height_static = $(".site-header.static").outerHeight(),
+        fitscreen = window_height - header_height;
+    $(".fullscreen").css("height", window_height)
+    $(".fitscreen").css("height", fitscreen);
 
-  /* -------- Active Sticky ---------- */
-  $(".default-header").sticky({topSpacing:0});
-     if(document.getElementById("default-select")){
-          $('select').niceSelect();
+    /* ----- Dynamic Window resize ----- */
+    window.addEventListener("resize", setWindowSize); //Register event on window resize
+    function setWindowSize() {
+        window_width = $(window).width(),
+            window_height = window.innerHeight,
+            header_height = $(".default-header").height(),
+            header_height_static = $(".site-header.static").outerHeight(),
+            fitscreen = window_height - header_height;
+        $(".fullscreen").css("height", window_height)
+        $(".fitscreen").css("height", fitscreen);
+    }
+
+
+    /* -------- Active Sticky ---------- */
+    $(".default-header").sticky({ topSpacing: 0 });
+    if (document.getElementById("default-select")) {
+        $('select').niceSelect();
     };
 
     $('.img-pop-up').magnificPopup({
         type: 'image',
-        gallery:{
-        enabled:true
+        gallery: {
+            enabled: true
         }
     });
-	$(window).load(function () {
-		$('#preload').fadeOut();
-	});
-	
+    $(window).load(function() {
+        $('#preload').fadeOut();
+    });
+
     $('.counter').counterUp({
         delay: 10,
         time: 1000
@@ -51,11 +51,11 @@ $(document).ready(function() {
     });
 
     $('.active-works-carousel').owlCarousel({
-        items:1,
-        loop:true,
+        items: 1,
+        loop: true,
         margin: 100,
         dots: true,
-        autoplay:true,
+        autoplay: true,
         responsive: {
             0: {
                 items: 1
@@ -70,14 +70,15 @@ $(document).ready(function() {
     });
 
     $('.active-gallery').owlCarousel({
-        items:1,
-        loop:true,
+        items: 1,
+        loop: true,
         dots: true,
-        autoplay:true,
-        nav:true,
+        autoplay: true,
+        nav: true,
         navText: ["<span class='lnr lnr-arrow-up'></span>",
-        "<span class='lnr lnr-arrow-down'></span>"],        
-            responsive: {
+            "<span class='lnr lnr-arrow-down'></span>"
+        ],
+        responsive: {
             0: {
                 items: 1
             },
@@ -94,7 +95,7 @@ $(document).ready(function() {
         }
     });
 
-$('.active-blog-slider').owlCarousel({
+    $('.active-blog-slider').owlCarousel({
         nav: false,
         loop: true,
         dots: true,
@@ -103,19 +104,50 @@ $('.active-blog-slider').owlCarousel({
         autoplayTimeout: 7000,
         smartSpeed: 1000,
         animateOut: 'fadeOut',
-        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-      })
-	
+        navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    })
 
-/* When the user clicks anywhere outside of the modal, close it */
-window.onclick = function(event) {
-	if(event.target.id == "705-modal" || event.target.id == "p4-modal" || event.target.id == "p3-modal" || event.target.id == "twltex-modal" || event.target.id == "ctrain-modal" || event.target.id == "msound-modal" || event.target.id == "284-hof-modal") {
-		$("#" + event.target.id).fadeOut("fast", function() { event.target.style.display = "none"; });
-	}
-}
+    // Select all links with hashes
+    $('.navbar-nav a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .on('click', function(event) {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - 50
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                }
+            }
+        });
 
-/* 13-2-2022 Update: Remove the mc_signup form or whatever that's called, does nothing. */
-$(document).ready(function() {
+
+    /* When the user clicks anywhere outside of the modal, close it */
+    window.onclick = function(event) {
+        if (event.target.id == "705-modal" || event.target.id == "p4-modal" || event.target.id == "p3-modal" || event.target.id == "twltex-modal" || event.target.id == "ctrain-modal" || event.target.id == "msound-modal" || event.target.id == "284-hof-modal") {
+            $("#" + event.target.id).fadeOut("fast", function() { event.target.style.display = "none"; });
+        }
+    }
+
+    /* 13-2-2022 Update: Remove the mc_signup form or whatever that's called, does nothing. */
+    $(document).ready(function() {
         let selectedTab = "notice";
 
         $('.filter-btn').click(function() {
@@ -125,12 +157,12 @@ $(document).ready(function() {
             /* Remove .selected from all elements with class filter-btn */
             $(".filter-btn").removeClass("selected")
             $(this).addClass("selected")
-            
+
             var list = document.getElementsByClassName("dlsection");
             for (var i = 0; i < list.length; i++) {
                 var elements = list.item(i)
 
-                if(!elements.classList.contains(val) && val != "null") {
+                if (!elements.classList.contains(val) && val != "null") {
                     $(elements).fadeOut(300)
                 } else {
                     $(elements).fadeIn(500)
@@ -144,19 +176,19 @@ $(document).ready(function() {
             $(".tab-btn").removeClass("selected")
             $(`.tab-btn`).each(function() {
                 var value = $(this).val();
-                if(value == selectedTab) $(this).addClass('selected')
+                if (value == selectedTab) $(this).addClass('selected')
             });
-            
+
             var list = document.getElementsByClassName("modalbody");
             for (var i = 0; i < list.length; i++) {
                 var elements = list.item(i)
 
-                if(!elements.classList.contains(selectedTab)) {
+                if (!elements.classList.contains(selectedTab)) {
                     $(elements).hide()
                 } else {
                     $(elements).show()
                 }
             }
         })
-	});   
+    });
 });
