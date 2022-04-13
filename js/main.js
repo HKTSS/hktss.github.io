fcf1844e914c9090158b2e111acf72cdf21418cb
LX862 <kenny.mh.hui@gmail.com>
2022-04-13 11:25:24 +0800

    Update more images, and several optimizations

--- js/main.js
+++ js/main.js
@@ -1,31 +1,28 @@
 $(document).ready(function() {
     "use strict";
-    var window_width = $(window).width(),
-        window_height = window.innerHeight,
-        header_height = $(".default-header").height(),
-        header_height_static = $(".site-header.static").outerHeight(),
-        fitscreen = window_height - header_height;
+    var window_width = $(window).width()
+    var window_height = window.innerHeight
+    var header_height = $(".default-header").height()
+    var header_height_static = $(".site-header.static").outerHeight()
+    var fitscreen = window_height - header_height;
     $(".fullscreen").css("height", window_height)
     $(".fitscreen").css("height", fitscreen);
 
     /* ----- Dynamic Window resize ----- */
-    window.addEventListener("resize", setWindowSize); //Register event on window resize
-    function setWindowSize() {
-        window_width = $(window).width(),
-            window_height = window.innerHeight,
-            header_height = $(".default-header").height(),
-            header_height_static = $(".site-header.static").outerHeight(),
-            fitscreen = window_height - header_height;
-        $(".fullscreen").css("height", window_height)
-        $(".fitscreen").css("height", fitscreen);
-    }
+    // window.addEventListener("resize", setWindowSize); //Register event on window resize
+    // function setWindowSize() {
+    //     window_width = $(window).width(),
+    //         window_height = window.innerHeight,
+    //         header_height = $(".default-header").height(),
+    //         header_height_static = $(".site-header.static").outerHeight(),
+    //         fitscreen = window_height - header_height;
+    //     $(".fullscreen").css("height", window_height)
+    //     $(".fitscreen").css("height", fitscreen);
+    // }
 
 
     /* -------- Active Sticky ---------- */
     $(".default-header").sticky({ topSpacing: 0 });
-    if (document.getElementById("default-select")) {
-        $('select').niceSelect();
-    };
 
     $('.img-pop-up').magnificPopup({
         type: 'image',
@@ -33,14 +30,17 @@ $(document).ready(function() {
             enabled: true
         }
     });
+
     $(window).load(function() {
         $('#preload').fadeOut();
     });
 
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
+    if ($('.counter').length) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    }
 
     $('.play-btn').magnificPopup({
         type: 'iframe',
@@ -49,63 +49,67 @@ $(document).ready(function() {
         preloader: false,
         fixedContentPos: false
     });
-
-    $('.active-works-carousel').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 100,
-        dots: true,
-        autoplay: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            480: {
-                items: 1,
-            },
-            768: {
-                items: 1,
+    if ($('.active-works-carousel').length) {
+        $('.active-works-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 100,
+            dots: true,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1,
+                },
+                768: {
+                    items: 1,
+                }
             }
-        }
-    });
+        });
+    }
 
-    $('.active-gallery').owlCarousel({
-        items: 1,
-        loop: true,
-        dots: true,
-        autoplay: true,
-        nav: true,
-        navText: ["<span class='lnr lnr-arrow-up'></span>",
-            "<span class='lnr lnr-arrow-down'></span>"
-        ],
-        responsive: {
-            0: {
-                items: 1
-            },
-            480: {
-                items: 1,
-            },
-            768: {
-                items: 2,
-            },
-            900: {
-                items: 6,
+
+    if ($('.active-gallery').length) {
+        $('.active-gallery').owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+            autoplay: true,
+            nav: true,
+            navText: ["<span class='lnr lnr-arrow-up'></span>", "<span class='lnr lnr-arrow-down'></span>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+                },
+                900: {
+                    items: 6,
+                }
+
             }
+        });
+    }
 
-        }
-    });
-
-    $('.active-blog-slider').owlCarousel({
-        nav: false,
-        loop: true,
-        dots: true,
-        items: 1,
-        autoplay: true,
-        autoplayTimeout: 7000,
-        smartSpeed: 1000,
-        animateOut: 'fadeOut',
-        navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
-    })
+    if ($('.active-blog-slider').length) {
+        $('.active-blog-slider').owlCarousel({
+            nav: false,
+            loop: true,
+            dots: true,
+            items: 1,
+            autoplay: true,
+            autoplayTimeout: 7000,
+            smartSpeed: 1000,
+            animateOut: 'fadeOut',
+            navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+        });
+    }
 
     // Select all links with hashes
     $('.navbar-nav a[href*="#"]')
@@ -147,75 +151,74 @@ $(document).ready(function() {
     }
 
     /* 13-2-2022 Update: Remove the mc_signup form or whatever that's called, does nothing. */
-    $(document).ready(function() {
-        let selectedTab = "notice";
+    let selectedTab = "notice";
 
-        $('.filter-btn').click(function() {
-            /* Get the button value */
-            let val = $(this).attr("value");
+    $('.filter-btn').click(function() {
+        /* Get the button value */
+        let val = $(this).attr("value");
 
-            /* Remove .selected from all elements with class filter-btn */
-            $(".filter-btn").removeClass("selected")
-            $(this).addClass("selected")
+        /* Remove .selected from all elements with class filter-btn */
+        $(".filter-btn").removeClass("selected")
+        $(this).addClass("selected")
 
-            var list = document.getElementsByClassName("dlsection");
-            for (var i = 0; i < list.length; i++) {
-                var elements = list.item(i)
+        var list = document.getElementsByClassName("dlsection");
+        for (var i = 0; i < list.length; i++) {
+            var elements = list.item(i)
 
-                if (!elements.classList.contains(val) && val != "null") {
-                    $(elements).fadeOut(300)
-                } else {
-                    $(elements).fadeIn(500)
-                }
-            }
-        })
-
-        $('.dlbutton').click(function() {
-            let url = $(this).attr("url")
-            let checkBoxTarget = $(this).attr("check")
-            if (url == undefined || url.length == 0) {
-                return;
-            }
-
-            if (!document.getElementById(checkBoxTarget).checked) {
-                alert('你必須同意以上事項，才能下載本檔案。');
-                return;
-            }
-
-            window.open(url, "_blank").focus();
-        })
-
-        $('#scrolltop').click(function() {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        })
-
-        $('.tab-btn').click(function() {
-            /* Get the button value */
-            selectedTab = $(this).attr("value");
-            $(".tab-btn").removeClass("selected")
-            $(`.tab-btn`).each(function() {
-                var value = $(this).val();
-                if (value == selectedTab) $(this).addClass('selected')
-            });
-
-            var list = document.getElementsByClassName("modalbody");
-            for (var i = 0; i < list.length; i++) {
-                var elements = list.item(i)
-
-                if (!elements.classList.contains(selectedTab)) {
-                    $(elements).hide()
-                } else {
-                    $(elements).show()
-                }
-            }
-        })
-
-        setInterval(() => {
-            if ($(window).scrollTop() <= 10) {
-                $('#scrolltop').css('opacity', '0.0');
+            if (!elements.classList.contains(val) && val != "null") {
+                $(elements).fadeOut(300)
             } else {
-                $('#scrolltop').css('opacity', '1.0');
+                $(elements).fadeIn(500)
             }
-        }, 100);
+        }
+    })
+
+    $('.dlbutton').click(function() {
+        let url = $(this).attr("url")
+        let checkBoxTarget = $(this).attr("check")
+        if (url == undefined || url.length == 0) {
+            return;
+        }
+
+        if (!document.getElementById(checkBoxTarget).checked) {
+            alert('你必須同意以上事項，才能下載本檔案。');
+            return;
+        }
+
+        window.open(url, "_blank").focus();
     });
+
+    $('#scrolltop').click(function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    $('.tab-btn').click(function() {
+        /* Get the button value */
+        selectedTab = $(this).attr("value");
+        $(".tab-btn").removeClass("selected")
+        $(`.tab-btn`).each(function() {
+            var value = $(this).val();
+            if (value == selectedTab) $(this).addClass('selected')
+        });
+
+        var list = document.getElementsByClassName("modalbody");
+        for (var i = 0; i < list.length; i++) {
+            var elements = list.item(i)
+
+            if (!elements.classList.contains(selectedTab)) {
+                $(elements).hide()
+            } else {
+                $(elements).show()
+            }
+        }
+    })
+
+    /* Looped every 100ms or 0.1 second */
+    setInterval(() => {
+        if ($(window).scrollTop() <= 10) {
+            $('#scrolltop').css('opacity', '0.0');
+        } else {
+            $('#scrolltop').css('opacity', '1.0');
+        }
+    }, 100);
 });
